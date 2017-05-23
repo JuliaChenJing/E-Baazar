d3b96e8b2bdeaf0dc0724c898ef9b80e2bc81ce8
VraisZaida <z.g.gebreslase@gmail.com>
2017-05-23 11:11:50 -0500

    bug free Spring

--- ebazzar/src/main/java/org/black/middleware/externalinterfaces/CreditVerification.java
+++
@@ -1,8 +0,0 @@
-package middleware.externalinterfaces;
-
-import middleware.exceptions.MiddlewareException;
-
-
-public interface CreditVerification {
-	public void checkCreditCard(CreditVerificationProfile profile) throws MiddlewareException;
-}
